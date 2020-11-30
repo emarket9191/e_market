a9f29a263cd14be445e60842791e001d21d7585a
YehuditYaacov <juditjaacov@com.com>
2020-11-30 08:21:15 -0800

    creat CRUD user

--- Modules/user-model.js
+++ Modules/user-model.js
@@ -4,11 +4,26 @@ const ObjectId = require('mongoose').Types.ObjectId;
 const user = new schema(
     {
     first_Name:{type:String,required:true},
-    last_Name :{type:String,required:true}
+    last_Name :{type:String,required:true},
+    user_Name : {type:String,required:true},
+    gender : {type:String},
+    birthday : {type:Date},
+    profileImage : { type: String,required: true},
+    business_name :{type:String},
+    password :{type:String,required:true},
+    email:{type:String,required:true}
+
+
 },
 {timestamps:true},
 
 
 )
+user.virtual('shops', {
+    ref: 'shop', 
+    localField: '_id', 
+    foreignField: 'shop_id', 
+  
+});
 
 module.exports = mongoose.model("users", user)
