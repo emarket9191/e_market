8d12f95f96d5e0e11d54195e0ac874f9a077f169
hagay kassa <hagaykassa@gmail.com>
2020-12-02 01:55:42 +0200

    drop-down list of categories

--- client/src/ProductManagement/ProductManagement.jsx
+++ client/src/ProductManagement/ProductManagement.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
@@ -8,6 +8,10 @@ import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import { grey } from "@material-ui/core/colors";
 import Paper from "@material-ui/core/Paper";
+import NativeSelects from "./DropDown"
+import { getAllCategories } from "../api/index";
+
+
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -40,7 +44,20 @@ const useStyles = makeStyles((theme) => ({
 }));
 export default function ProductManagement() {
   const classes = useStyles();
-  const [controls, setControls] = React.useState({});
+  const [controls, setControls] =useState({});
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState([]);
+
+
+  const getCategories = async () => {
+    const response = await getAllCategories();
+    const array = response.data.data;
+    setCategories(array);
+    const x= 1;
+  };
+  useEffect(() => {
+    getCategories();
+  }, []);
   return (
     <div>
       <h1 className="ProductManagementTitle">Product Management</h1>
@@ -90,39 +107,9 @@ export default function ProductManagement() {
             <SearchIcon />
           </IconButton>
         </Paper>
-        <Paper component="form" className={classes.root}>
-          <InputBase
-            className={classes.input}
-            placeholder="search category"
-            inputProps={{ "aria-label": "search category" }}
-          />
 
-          <Divider className={classes.divider} orientation="vertical" />
-          <IconButton
-            type="submit"
-            className={classes.iconButton}
-            aria-label="search"
-          >
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-        <Paper component="form" className={classes.root}>
-          <InputBase
-            className={classes.input}
-            placeholder="search subcategory"
-            inputProps={{ "aria-label": "search category" }}
-          />
-
-          <Divider className={classes.divider} orientation="vertical" />
-          <IconButton
-            type="submit"
-            className={classes.iconButton}
-            aria-label="search"
-          >
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-        <Button
+{categories.length>0&&<NativeSelects props={categories}/>}
+       <Button
           id="AddProductButton"
           className={classes.button}
           variant="contained"
