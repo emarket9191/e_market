470ab72d787998bbb72d1301cdf8c15c8b6745db
unknown <you@example.com>
2020-12-02 04:49:57 +0200

    sign in post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -14,6 +14,10 @@ const api = axios.create({
   baseURL: generateFullUrl() + "/api",
 });
 export const getAllProducts = () => api.get("/product");
+// export const signin = (email,password)=> api.post('/user/auth',(email,password));
+
+export const signin = (email,password)=> api.post('/user/auth', {email,password})
+
 // export const searchGuideById = (id) => api.get(`guides/:${id}`);
 // export const searchGuides = (country, lang, city, cost, comments) =>
 //   api.get(
@@ -27,7 +31,7 @@ export const getAllProducts = () => api.get("/product");
 //   );
 
 // const apis = { getAllGuides, searchGuides,searchGuideById };
-const apis = {getAllProducts};
+const apis = {getAllProducts,signin};
 
 export default apis;
 
